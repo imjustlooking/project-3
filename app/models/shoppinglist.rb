ed158f0eef80ea87b6e0bcd4d180a4a8f0acbbc4
thamjieying <thamjieying@gmail.com>
2017-11-07 14:41:09 +0800

    authentication devics setup

--- app/models/shoppinglist.rb
+++ app/models/shoppinglist.rb
@@ -1,3 +1,4 @@
 class Shoppinglist < ApplicationRecord
   belongs_to :user_id
+  has_many :items
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,3 +1,4 @@
 class Stock < ApplicationRecord
   belongs_to :category
+  has_many :items
 end
