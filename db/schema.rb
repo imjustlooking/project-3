d482ed486a0174e501437d75f055c7a55b9c902f
Gabriel <gabrielulu@gmail.com>
2017-11-07 17:43:16 +0800

    testing edits

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107062231) do
+ActiveRecord::Schema.define(version: 20171106102429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,36 +19,6 @@ ActiveRecord::Schema.define(version: 20171107062231) do
     t.string "name_category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "items", force: :cascade do |t|
-    t.bigint "stock_id"
-    t.integer "quantity_ordered"
-    t.bigint "shoppinglist_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["shoppinglist_id"], name: "index_items_on_shoppinglist_id"
-    t.index ["stock_id"], name: "index_items_on_stock_id"
-  end
-
-  create_table "shoppinglists", force: :cascade do |t|
-    t.string "name_shoppinglist"
-    t.datetime "paid_on"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_shoppinglists_on_user_id"
-  end
-
-  create_table "stocks", force: :cascade do |t|
-    t.string "name_item"
-    t.integer "barcode"
-    t.decimal "price"
-    t.integer "quantity"
-    t.bigint "category_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_stocks_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -66,8 +36,4 @@ ActiveRecord::Schema.define(version: 20171107062231) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "items", "shoppinglists"
-  add_foreign_key "items", "stocks"
-  add_foreign_key "shoppinglists", "users"
-  add_foreign_key "stocks", "categories"
 end
