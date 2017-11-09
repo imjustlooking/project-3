dfda7b8680510a57475bcb002b7a27b1021367ec
thamjieying <thamjieying@gmail.com>
2017-11-09 15:03:30 +0800

    updated show view for shopping list

--- app/controllers/shoppinglists_controller.rb
+++ app/controllers/shoppinglists_controller.rb
@@ -1,16 +1,29 @@
 class ShoppinglistsController < ApplicationController
   def index
     @new_shoppinglist = Shoppinglist.new
+    @shoplists = current_user.shoppinglists
   end
 
   def show
+    @shoppinglist = Shoppinglist.find(params[:id])
 
-  end 
+  end
   def create
     current_user.shoppinglists.create(params.require(:shoppinglist).permit(:name_shoppinglist))
-    redirect_to action: "show", id: params[:id]
+    # render :json => current_user.shoppinglists
+    redirect_to action: "index"
   end
   def new
     @new_shoppinglist = Shoppinglist.new
+  end
+
+  def edit
+    @shoppinglist = Shoppinglist.find(params[:id])
+  end
+
+  def update
+    @shoppinglist.update(params.require(:shoppinglist).permit(:name_shoppinglist))
+  end
+  def destroy
   end
 end
