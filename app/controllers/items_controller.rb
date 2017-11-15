d2677c2ce4cd54f0694c0d738de9fc53d68cbb67
thamjieying <thamjieying@gmail.com>
2017-11-15 16:32:39 +0800

    updating adding shopping lists

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,29 @@
 class ItemsController < ApplicationController
   def create
-    @item = Item.create(item_params)
+    if params[:item][:shoppinglists].present?
+      # create the shopping list first
+      shoppinglist = Shoppinglist.create(
+        name_shoppinglist: params[:item][:shoppinglists][:name_shoppinglist],
+        user_id: current_user.id
+      )
+    else
+      shoppinglist = Shoppinglist.find(params[:item][:shoppinglist_id])
+    end
+
+    # render json: params
+    @item = Item.new(item_params)
+    @item.shoppinglist_id = shoppinglist.id
     if @item.save
       flash[:success] = @item.quantity_ordered.to_s + ' unit(s) of ' + @item.stock.name_item + ' has been added to "' + @item.shoppinglist.name_shoppinglist + '".'
       redirect_to stocks_path
     else
-      flash[:danger] = 'Item add failed. Please check if you have selected a valid shopping list.'
+      render json: {
+        shoppinglist: shoppinglist,
+        errors: @item.errors.messages
+      }
+      # flash[:danger] = 'Item add failed. Please check if you have selected a valid shopping list.'
       # you guys can change the alert tag accordingly: https://getbootstrap.com/docs/4.0/components/alerts/
-      redirect_to stocks_path
+      # redirect_to stocks_path
     end
   end
 
@@ -44,6 +60,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:stock_id, :quantity_ordered, :shoppinglist_id)
+    params.require(:item).permit(:stock_id, :quantity_ordered, shoppinglists_attributes: [:name_shoppinglist])
   end
 end
